4cc0f5f19351bc360e5e85956225989d4112619f
Jennifer Tieu <jennifer.tieuu@gmail.com>
2021-12-05 20:08:41 -0600

    remove password hash import

--- week10/project/app.py
+++ week10/project/app.py
@@ -3,7 +3,6 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology
 
